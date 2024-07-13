97e03b5f9f082e3150a5130d707e231a4fb5db51
VictorFrancelino <victor.francelino10@gmail.com>
2024-07-13 01:59:34 -0400

    Adicionado um banner com texto de apresentação e imagem do anime Demon Slayer na página inicial; implementado um carrossel de imagens que exibe os personagens do anime.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
 import './App.css';
 import NavbarDemonSlayer from './components/navbar';
 import Banner from './components/banner';
+import PersonagensCarousel from './components/personagensCarousel';
+import Onis from './components/onis';
 
 function App() {
   return (
     <div className="App">
-      <NavbarDemonSlayer/>
-      <Banner/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarDemonSlayer />
+      <Banner />
+      <PersonagensCarousel />
+      <Onis/>
+      <header className="App-header"></header>
     </div>
   );
 }
---
+++ src/components/onis.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './onis.css';
+
+const Onis = () => {
+    return(
+        <div className="onis-faixa mt-4">
+            <h2>Os Onis</h2>
+        </div>
+    );
+};
+
+export default Onis;
